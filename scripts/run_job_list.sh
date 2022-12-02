8c4ffc3e669be3551b294a6ad6b0d0dc7ec5398a
Aleksandr Kazachkov <akazachk@users.noreply.github.com>
2022-12-02 18:23:22 -0500

    Commented out irrelevant parts of run_job_list.sh

--- scripts/run_job_list.sh
+++ scripts/run_job_list.sh
@@ -9,11 +9,11 @@
 #SBATCH --ntasks=1                   # Number of tasks to run
 #SBATCH --cpus-per-task=1            # Number of CPU cores per task
 #SBATCH --mem=7.8gb                  # Total memory limit
-#SBATCH --time=4-00:00:00            # Time limit
+#SBATCH --time=5-00:00:00            # Time limit
 #SBATCH --array=1-6420
 
 ##SBATCH --output=/blue/akazachkov/ambareesh.vaidya/cp_outcome/paper_log/output_%a-%A.log
-##SBATCH --output=slurm_log/output_%a-%A.log
+##SBATCH --output=output_%a-%A.log
 #SBATCH --output=slurm_%x_%A-%a.log  # standard output log (%x = job name, %A = job id, %a = array index)
 #SBATCH --mail-type=END,FAIL         # mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=ambareesh.vaidya@ufl.edu
@@ -28,9 +28,9 @@ pwd; hostname; date
 #########################
 ## Constants
 PROJ_DIR="/home/ambareesh.vaidya/repos/cp_outcome"
-CASE_NUM=`printf %04d $SLURM_ARRAY_TASK_ID`
-DATESTUB=`date +%F`
-RESULTS_DIR="/blue/akazachkov/ambareesh.vaidya/cp_outcome/paper/results/$DATESTUB"
+#CASE_NUM=`printf %04d $SLURM_ARRAY_TASK_ID`
+#DATESTUB=`date +%F`
+#RESULTS_DIR="/blue/akazachkov/ambareesh.vaidya/cp_outcome/paper/results/$DATESTUB"
 
 #########################
 ## Arguments
@@ -51,6 +51,7 @@ conda activate python38
 ## Prepare run command
 echo ""
 echo "Starting ${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID}"
+# *** CMD is read in here
 CMD=$(sed -n "${SLURM_ARRAY_TASK_ID}p" ${JOB_LIST})
 
 echo ""
