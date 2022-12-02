c6f047837320b9ab158dac6ba9d79eca93946818
Aleksandr Kazachkov <akazachk@users.noreply.github.com>
2022-12-02 11:12:46 -0500

    Updated run script to have clearer variables

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,33 +1,44 @@
 #!/bin/bash
 
 #SBATCH --job-name=cp_outcome
-#SBATCH --mail-type=END,FAIL
-#SBATCH --mail-user=ambareesh.vaidya@ufl.edu
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
-#SBATCH --mem=8gb
-#SBATCH --time=2-00:00:00
-#SBATCH --output=/blue/akazachkov/ambareesh.vaidya/cp_outcome/paper_log/output_%a-%A.log
-#SBATCH --array=1-426
+#SBATCH -p hpg-milan
 #SBATCH --account=akazachkov
 #SBATCH --qos=akazachkov-b
+#SBATCH --nodes=1                    # Use one node
+#SBATCH --ntasks=1                   # Run a single task        
+#SBATCH --cpus-per-task=64           # Number of CPU cores per task
+#SBATCH --mem=8gb                    # Total memory limit
+#SBATCH --time=2-00:00:00            # Time limit
+
+##SBATCH --output=/blue/akazachkov/ambareesh.vaidya/cp_outcome/paper_log/output_%a-%A.log
+#SBATCH --output=output_%a-%A.log
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=ambareesh.vaidya@ufl.edu
+
+#SBATCH --array=1-426
 
 pwd; hostname; date
 
-CUTS="1"
-if [ "$1" = 0 ]; then
+#########################
+## Arguments
+if [ -z $1 ]; then
+  #CUTS="0 1"
+  CUTS="1"
+elif [ "$1" = 0 ]; then
   CUTS="0"
 elif [ "$1" = 1 ]; then
   CUTS="1"
 elif [ "$1" = 2 ]; then
   CUTS="0 1"
-elif [ ! -z $1 ]; then
+else
   echo "First argument must be a 0 (cuts off), a 1 (cuts on), a 2 (both), or empty (default cuts on)."
   exit 1
 fi
 
-RANDOM_SEED="2"
-if [ "$2" = 1 ]; then
+if [ -z $2 ]; then
+  #RANDOM_SEED="2 4 8 16 32"
+  RANDOM_SEED="2"
+elif [ "$2" = 1 ]; then
   RANDOM_SEED="2"
 elif [ "$2" = 2 ]; then
   RANDOM_SEED="2 4"
@@ -37,37 +48,43 @@ elif [ "$2" = 4 ]; then
   RANDOM_SEED="2 4 8 16"
 elif [ "$2" = 5 ]; then
   RANDOM_SEED="2 4 8 16 32"
-elif [ ! -z $2 ]; then
+else
   echo "Second argument must be a 1 (one random seed), a 2 (two random seeds), a 3 (three random seeds), a 4 (four random seeds), a 5 (five random seeds) or empty (default random seeds equals to 1)."
   exit 1
 fi
 
+#########################
+## Constants
+PROJ_DIR="/home/ambareesh.vaidya/repos/cp_outcome"
+TYPE="solved"
+INSTANCE_FILE="$PROJ_DIR/data/$TYPE.instances"
+CASE_NUM=`printf %04d $SLURM_ARRAY_TASK_ID`
+DATESTUB=`date +%F`
+RESULTS_DIR="/blue/akazachkov/ambareesh.vaidya/cp_outcome/paper/results/$DATESTUB"
+EXECUTABLE="${PROJ_DIR}/src/fullrun_new.py"
+
+#########################
+## Modules
 module load conda
 conda activate python38
 
-TYPE="solved"
-CASE_NUM=`printf %04d $SLURM_ARRAY_TASK_ID`
+#########################
+## Run code
+for CUT_MODE in $CUTS; do 
+  for SEED in $RANDOM_SEED; do
+    CURR_RESULT_DIR="${RESULTS_DIR}/cut_${CUT_MODE}/seed_${SEED}/${CASE_NUM}"
+    mkdir -p $CURR_RESULT_DIR
 
-for i in $CUTS; do 
-  CUT_MODE=$i 
-  for j in $RANDOM_SEED; do
-    SEED=$j 
-    
-    mkdir -p /blue/akazachkov/ambareesh.vaidya/cp_outcome/paper/results/cut_${CUT_MODE}/seed_${SEED}/${CASE_NUM}
-    INSTANCE_FILE=${TYPE}.instances
-    RESULT_DIR="/blue/akazachkov/ambareesh.vaidya/cp_outcome/paper/results//cut_${CUT_MODE}/seed_${SEED}/${CASE_NUM}"
+    INST=$(sed -n "${SLURM_ARRAY_TASK_ID}p" $INSTANCE_FILE)
 
-    echo "Running task ${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID} in parallel mode at `date`"
-    INST=$(sed -n "${SLURM_ARRAY_TASK_ID}p" /home/ambareesh.vaidya/repos/cp_outcome/data/${INSTANCE_FILE})
+    echo "Running task ${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID} for instance $INST in parallel mode at `date` with results printed to $CURR_RESULT_DIR"
     FILE="/home/ambareesh.vaidya/miplib2017/${INST}"
-    
     	
-    echo ${INST}
-    echo ${CASE_NUM}
-    echo cut_${CUT_MODE}
-    echo seed_${SEED}
+    echo "Instance: ${INST}"
+    echo "Case: ${CASE_NUM}"
+    echo "Cut mode: cut_${CUT_MODE}"
+    echo "Seed: seed_${SEED}"
+    python $EXECUTABLE --instance ${FILE} --cuts ${CUT_MODE} --result_path ${RESULT_DIR} --seed ${SEED}
 
-    python /home/ambareesh.vaidya/repos/cp_outcome/src/fullrun_new.py --instance ${FILE} --cuts ${CUT_MODE} --result_path ${RESULT_DIR} --seed ${SEED}
-
-  done
-done
+  done # loop over RANDOM_SEED
+done # loop over SEEDS
