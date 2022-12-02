cb032dee15403ac8b264f9c74327ccf219bc755e
Ambareesh Vaidya <47357589+ambareeshvaidya@users.noreply.github.com>
2022-12-01 19:20:49 -0500

    Add files via upload

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,9 +6,11 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=8gb
-#SBATCH --time=22:00:00
-#SBATCH --output=/blue/akazachkov/ambareesh.vaidya/cp_outcome/log/output_%A-%a.log
-#SBATCH --array=170
+#SBATCH --time=2-00:00:00
+#SBATCH --output=/blue/akazachkov/ambareesh.vaidya/cp_outcome/paper_log/output_%a-%A.log
+#SBATCH --array=1-426
+#SBATCH --account=akazachkov
+#SBATCH --qos=akazachkov-b
 
 pwd; hostname; date
 
@@ -30,11 +32,11 @@ if [ "$2" = 1 ]; then
 elif [ "$2" = 2 ]; then
   RANDOM_SEED="2 4"
 elif [ "$2" = 3 ]; then
-  RADOM_SEED="2 4 8"
+  RANDOM_SEED="2 4 8"
 elif [ "$2" = 4 ]; then
   RANDOM_SEED="2 4 8 16"
 elif [ "$2" = 5 ]; then
-  RADOM_SEED="2 4 8 16 32"
+  RANDOM_SEED="2 4 8 16 32"
 elif [ ! -z $2 ]; then
   echo "Second argument must be a 1 (one random seed), a 2 (two random seeds), a 3 (three random seeds), a 4 (four random seeds), a 5 (five random seeds) or empty (default random seeds equals to 1)."
   exit 1
@@ -43,7 +45,7 @@ fi
 module load conda
 conda activate python38
 
-TYPE="final"
+TYPE="solved"
 CASE_NUM=`printf %04d $SLURM_ARRAY_TASK_ID`
 
 for i in $CUTS; do 
@@ -51,9 +53,9 @@ for i in $CUTS; do
   for j in $RANDOM_SEED; do
     SEED=$j 
     
-    mkdir -p /blue/akazachkov/ambareesh.vaidya/cp_outcome/results/$(date +"%d-%m-%Y")/cut_${CUT_MODE}/seed_${SEED}/${CASE_NUM}
+    mkdir -p /blue/akazachkov/ambareesh.vaidya/cp_outcome/paper/results/cut_${CUT_MODE}/seed_${SEED}/${CASE_NUM}
     INSTANCE_FILE=${TYPE}.instances
-    RESULT_DIR="/blue/akazachkov/ambareesh.vaidya/cp_outcome/results/$(date +"%d-%m-%Y")/cut_${CUT_MODE}/seed_${SEED}/${CASE_NUM}"
+    RESULT_DIR="/blue/akazachkov/ambareesh.vaidya/cp_outcome/paper/results//cut_${CUT_MODE}/seed_${SEED}/${CASE_NUM}"
 
     echo "Running task ${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID} in parallel mode at `date`"
     INST=$(sed -n "${SLURM_ARRAY_TASK_ID}p" /home/ambareesh.vaidya/repos/cp_outcome/data/${INSTANCE_FILE})
@@ -65,7 +67,7 @@ for i in $CUTS; do
     echo cut_${CUT_MODE}
     echo seed_${SEED}
 
-    python /home/ambareesh.vaidya/repos/cp_outcome/src/fullrun.py --instance ${FILE} --cuts ${CUT_MODE} --result_path ${RESULT_DIR} --seed ${SEED}
+    python /home/ambareesh.vaidya/repos/cp_outcome/src/fullrun_new.py --instance ${FILE} --cuts ${CUT_MODE} --result_path ${RESULT_DIR} --seed ${SEED}
 
   done
 done
